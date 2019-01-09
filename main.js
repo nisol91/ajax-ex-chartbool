60453d2cca679d04bfaacbac754187c48d02e72b
nisol91 <nicolasolzi91@gmail.com>
2019-01-09 22:57:48 +0100

    2 [grafico a torta quasi finito]

--- main.js
+++ main.js
@@ -1,34 +1,100 @@
 $(document).ready(function() {
 
+  var urlApi = 'http://157.230.17.132:4017/sales';
 
 
-//TORTA
-  var ctx_pie = $('#myChart_pie');
+  function cleanData (obj_data) {
 
-  var chartPie = new Chart(ctx_pie, {
-    // The type of chart we want to create
-    type: 'pie',
+    var newObject = {}
+    for (var i = 0; i < obj_data.length; i++) {
+      console.log(obj_data[i]['salesman']);
+      console.log(obj_data[i]['amount']);
 
-    // The data for our dataset
-    data: {
-      // labels: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto"],
-      labels: [
-        'lightblue',
-        'lightcoral',
-        'yellow'
-      ],
-      datasets: [{
-        label: ['green'],
-        backgroundColor: ['lightblue', 'lightcoral', 'yellow'],
-        // backgroundColor: 'rgb(255, 99, 132, .5)',
-        // borderColor: 'rgb(255, 99, 132)',
-        data:[10],
-      }]
-    },
-    // Configuration options go here
-    options: {}
-  });
-//--------------
+
+      //questa e' la parte importante: creo un oggetto che mi raggruppa i nomi uguali e mi somma i singoli amount di ogni valore
+      if (newObject[obj_data[i]['salesman']] == undefined) {
+        newObject[obj_data[i]['salesman']] = 0;
+      }
+      //IMPORTANTISSIMO: in pratica questo if mi dice che: andando a ciclare sui vari oggetti , se salesman non esiste(==undefined), allora
+      //lo vado a posizionare creandolo (cioe ponendo =0) nel nuovo oggetto newObj.
+
+      //poi vado a sommare l amount
+      newObject[obj_data[i]['salesman']] += obj_data[i]['amount']
+
+    }
+
+
+    var arrLabels = [];
+    var arrData = []
+    for (var key in newObject) {
+        arrLabels.push(key);
+        arrData.push(newObject[key]);
+    }
+    console.log(arrLabels);
+    console.log(arrData);
+    console.log(newObject);
+    //per far uscire da questa funzione i valori ottenuti devo per forza usare return
+    //Questa funzione mi ritorna un oggetto.
+    return {
+      labels: arrLabels,
+      data: arrData,
+    }
+  }
+
+  function getData() {
+    $.ajax({
+      url: urlApi,
+      method: 'GET',
+      success: function(data) {
+        console.log(data);
+        cleanData(data)
+        // console.log(cleanData(data).labels);
+        // console.log(cleanData(data).data);
+        //****************
+        //****GRAFICI*****
+        //****************
+
+        //TORTA
+        var ctx_pie = $('#myChart_pie');
+
+        var chartPie = new Chart(ctx_pie, {
+          // The type of chart we want to create
+          type: 'pie',
+
+          // The data for our dataset
+          data: {
+            // labels: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto"],
+            labels: cleanData(data).labels,
+            datasets: [{
+              // label: ,
+              backgroundColor: ['lightblue', 'lightcoral', 'yellow'],
+              // backgroundColor: 'rgb(255, 99, 132, .5)',
+              // borderColor: 'rgb(255, 99, 132)',
+              data: cleanData(data).data,
+            }]
+            //--------------
+          },
+          // Configuration options go here
+          options: {}
+        });
+      },
+      error: function() {
+        alert('errore');
+      }
+    })
+  }
+
+  getData()
+
+
+
+
+
+
+
+
+
+  
 //GRAFICO A LINEA
   var ctx_2 = $('#lineChart');
   var chart_2 = new Chart(ctx_2, {
